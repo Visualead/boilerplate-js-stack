245be0c115a561c86e96c2d9a1ff93eff6d288da
Ilan Chemla <ilanc@visualead.com>
2016-11-02 14:21:49 +0200

    Reformat code style in gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,31 +3,31 @@ import babel from 'gulp-babel';
 import eslint from 'gulp-eslint';
 
 const config = {
-    paths: {
-        js: {
-            src: 'src/**/*.js',
-            lib: 'lib/'
-        }
+  paths: {
+    js: {
+      src: 'src/**/*.js',
+      lib: 'lib/'
     }
+  }
 };
 
 gulp.task('babel-src', ['lint-src'], () =>
-    gulp.src(config.paths.js.src)
-        .pipe(babel())
-        .pipe(gulp.dest(config.paths.js.lib))
+  gulp.src(config.paths.js.src)
+    .pipe(babel())
+    .pipe(gulp.dest(config.paths.js.lib))
 );
 
 gulp.task('lint-src', () =>
-    gulp.src(config.paths.js.src)
-        .pipe(eslint())
-        .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
-        .pipe(eslint.failAfterError())
+  gulp.src(config.paths.js.src)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
+    .pipe(eslint.failAfterError())
 );
 
 gulp.task('watch', () => {
-    gulp.watch(config.paths.js.src, ['babel-src', 'test']);
+  gulp.watch(config.paths.js.src, ['babel-src', 'test']);
 });
 
 gulp.task('default', ['watch', 'babel-src']);
