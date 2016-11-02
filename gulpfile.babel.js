5d85f9fe8d2541a8c222771d81f6785075ec2f78
Ilan Chemla <ilanc@visualead.com>
2016-11-02 11:33:05 +0200

    Refactor babel task to include lint code check

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,19 +1,33 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import eslint from 'gulp-eslint';
 
-const paths = {
-    allSrcJs: 'src/**/*.js',
-    libDir: 'lib',
+const config = {
+    paths: {
+        js: {
+            src: 'src/**/*.js',
+            lib: 'lib/'
+        }
+    }
 };
 
-gulp.task('babel', () => {
-    return gulp.src(paths.allSrcJs)
+gulp.task('babel-src', ['lint-src'], () =>
+    gulp.src(config.paths.js.src)
         .pipe(babel())
-        .pipe(gulp.dest(paths.libDir));
+        .pipe(gulp.dest(config.paths.js.lib))
+);
+
+gulp.task('lint-src', () =>
+    gulp.src(config.paths.js.src)
+        .pipe(eslint())
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError())
+);
+
+gulp.task('watch', () => {
+    gulp.watch(config.paths.js.src, ['babel-src', 'test']);
 });
 
-gulp.task('watch', function() {
-    gulp.watch([paths.allSrcJs], ['babel']);
-});
-
-gulp.task('default', ['watch', 'babel']);
+gulp.task('default', ['watch', 'babel-src']);
